5ff9bdc454a3a0967a5f104484a64f1d2ad451a4
Lucas Mauro <lucas_de_mauro@hotmail.com>
2020-05-11 18:04:54 -0300

    Criando Rotas

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+// React.FunctionComponent e React.FC são a mesma coisa, deixa menos verboso
+const Dashboard: React.FunctionComponent = () => {
+  return <h1>Dashboard</h1>;
+};
+
+export default Dashboard;
